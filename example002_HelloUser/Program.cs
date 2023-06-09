f43b3250da5077803020724e928b0ec90436892c
Evg_375 <zsoroko25@gmail.com>
2023-06-09 19:48:02 +0300

    save all

--- example002_HelloUser/Program.cs
+++ example002_HelloUser/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
 //123
