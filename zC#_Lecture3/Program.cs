a2021b0f022b7b888871622f8bd8e61f58de5ad3
Evg_375 <zsoroko25@gmail.com>
2023-06-20 12:03:22 +0300

    ALL save

---
+++ zC#_Lecture3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Редактор текст
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+    
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) 
+//         result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+
+
+// Сортировка массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectioSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array [minPosition]) minPosition = j; 
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectioSort(arr);
+
+PrintArray(arr);
+
